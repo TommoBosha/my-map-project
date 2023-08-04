eac8466b330c07b1f1e8c4b29ef96c5a0cb39453
TommoBosha <116563699+TommoBosha@users.noreply.github.com>
2023-08-04 11:21:46 +0300

    test google maps

--- src/App.js
+++ src/App.js
@@ -1,25 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import GoogleMapComponent from "./components/GoogleMapComponent";
+
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div >
+      <GoogleMapComponent />
     </div>
   );
 }
 
 export default App;
+
+
+// AIzaSyBCG1kXqrS2nHI4TwwNXtbIBF4_AWPRpOg
---
+++ src/components/GoogleMapComponent.jsx
@@ -0,0 +1,37 @@
+import React from 'react';
+import { GoogleMap, LoadScript, Marker } from '@react-google-maps/api';
+
+
+const mapContainerStyle = {
+  width: '500px',
+  height: '402px',
+};
+
+const center = {
+  lat: 50.40363171032715,
+  lng: 30.3396692276001, 
+};
+
+const firstOficce = { lat: 50.40755844116211, lng: 30.338287353515625 };
+const secondOffice = {lat: 50.39970397949219, lng: 30.34105110168457 }
+
+
+const GoogleMapComponent = () => {
+
+
+  return (
+    <LoadScript googleMapsApiKey={process.env.REACT_APP_MAPS_API_KEY}>
+      <GoogleMap mapContainerStyle={mapContainerStyle} center={center} zoom={14}>
+        <Marker position={center} />
+        <Marker position={firstOficce} />
+        <Marker position={secondOffice} />
+        
+      
+      </GoogleMap>
+    </LoadScript>
+  );
+};
+
+export default GoogleMapComponent;
+
+
